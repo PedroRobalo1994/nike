5e6881128637a734c357409416e3a8f6590eff9c
Pedro Robalo <35338193+PedroRobalo1994@users.noreply.github.com>
2024-02-13 21:20:52 +0000

    Add new components and update file paths

--- src/sections/index.js
+++ src/sections/index.js
@@ -1,10 +1,19 @@
-import CustomerReviews from "./CustomerReviews";
-import Footer from "./Footer";
 import Hero from "./Hero";
 import PopularProducts from "./PopularProducts";
+import SuperQuality from "./SuperQuality";
 import Services from "./Services";
 import SpecialOffer from "./SpecialOffer";
+import CustomerReviews from "./CustomerReviews";
 import Subscribe from "./Subscribe";
-import SuperQuality from "./SuperQuality";
+import Footer from "./Footer";
 
-export { CustomerReviews, Footer, Hero, PopularProducts, Services, SpecialOffer, Subscribe, SuperQuality };
+export {
+    Hero,
+    PopularProducts,
+    SuperQuality,
+    Services,
+    SpecialOffer,
+    CustomerReviews,
+    Subscribe,
+    Footer
+}
